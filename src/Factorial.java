c8b6aec81fd37b4ad450b6138a05eec2193cd677
MiroFerko <miroslav.ferko1@gmail.com>
2022-09-25 21:11:08 +0200

    Factorial

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Factorial {
 
 
+
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number up to 16:");
